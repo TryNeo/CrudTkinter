c9e3fad250aa16c47b63a05602aaffd36ea7d215
josu3 <tshadez@hotmail.com>
2020-10-28 23:10:48 -0500

    Nuevo Estilo + Nuevo search

--- menus/categoria.py
+++ menus/categoria.py
@@ -5,7 +5,7 @@ from tkinter import ttk, messagebox
 class MainWindowC:
     def __init__(self, root):
         root.title('Categoria')
-        root.geometry("700x800")
+        root.geometry("700x450")
         root.resizable(0, 0)
 
         #########centered screen##############
@@ -22,11 +22,15 @@ class MainWindowC:
         root.deiconify()
 
         self.search = tk.StringVar()
-        label_search = tk.Label(root, text="Buscador:", font=("Arial", 12)).place(x=15, y=50)
-        entry_search = tk.Entry(root, width=30, textvariable=self.search).place(x=100, y=50)
-        button_search = tk.Button(root, text="Buscar", font=("Arial", 12),command=self.list_search).place(x=350, y=45)
+        
+        wrapper_category = tk.LabelFrame(root,text="Listado de Categoria")
+        wrapper_category.pack(fill="both",expand="yes",padx=20,pady=100)
+        self.my_tree_cate  = ttk.Treeview(wrapper_category,columns=(1,2,3),show="headings",height="10")
+        self.my_tree_cate.pack()
+        self.my_tree_cate.heading(1,text="ID")
+        self.my_tree_cate.heading(2,text="NOMBRE")
+        self.my_tree_cate.heading(3,text="DESCRIPCION")
 
-        self.registry_category(root)
         self.list_category(root)
 
     def connecting(self):
@@ -42,39 +46,38 @@ class MainWindowC:
             print("connection successfully")
     
     def list_search(self):
-        search = self.search.get()
-        print(search)
-
-    def list_update(self,my_tree_cate=None):
         self.connecting()
-        search = self.search.get()
-        if search:
-            query =  "SELECT id_categoria,nombre,descripcion FROM categoria WHERE nombre = %s"
-            get_data = (search,)
-            self.mysqlcursor.execute(query,get_data)
-            rows = self.mysqlcursor.fetchall()
-        else:
-            query = "SELECT id_categoria,nombre,descripcion FROM categoria"
-            self.mysqlcursor.execute(query)
-            rows = self.mysqlcursor.fetchall()
-        my_tree_cate.delete(*my_tree_cate.get_children())
+        search_list = self.search.get()
+        query = "SELECT id_categoria,nombre,descripcion FROM categoria WHERE nombre=%s"
+        parameters=(search_list)
+        self.mysqlcursor.execute(query,parameters)
+        rows = self.mysqlcursor.fetchall()
+        print(rows)
+        self.list_update(rows)
+    
+    def list_update(self,rows):
+        self.my_tree_cate.delete(*self.my_tree_cate.get_children())
         for i in rows:
-            my_tree_cate.insert('','end',values=i)
+            self.my_tree_cate.insert('','end',values=i)
+    
+    def list_clear(self):
+        query = "SELECT id_categoria,nombre,descripcion FROM categoria"
+        self.mysqlcursor.execute(query)
+        rows = self.mysqlcursor.fetchall()
+        self.list_update(rows)
 
-
-    def registry_category(self,root):
-        wrapper_category_reg = tk.LabelFrame(root,text="Categoria")
-        wrapper_category_reg.pack(fill="both",expand="yes",padx=20,pady=100)
-        title_label = tk.Label(wrapper_category_reg, text="Registro de usuarios", font=("Arial", 10), fg="black").place(x=90,
-                                                                                                         y=0)
-
-
+    
     def list_category(self,root):
-        wrapper_category = tk.LabelFrame(root,text="Listado de Categoria")
-        wrapper_category.pack(fill="both",expand="yes",padx=20,pady=100)
-        my_tree_cate  = ttk.Treeview(wrapper_category,columns=(1,2,3),show="headings",height="6")
-        my_tree_cate.pack()
-        my_tree_cate.heading(1,text="ID")
-        my_tree_cate.heading(2,text="NOMBRE")
-        my_tree_cate.heading(3,text="DESCRIPCION")
-        self.list_update(my_tree_cate)
+        self.connecting()
+        label_search = tk.Label(root, text="Buscador:", font=("Arial", 12)).place(x=15, y=20)
+        entry_search = tk.Entry(root, width=20, textvariable=self.search).place(x=100, y=20)
+        button_search = tk.Button(root, text="Buscar", font=("Arial", 12),command=self.list_search).place(x=280, y=15)
+        button_list_clear = tk.Button(root, text="restaurar listado", font=("Arial", 12),command=self.list_clear).place(x=370, y=15)
+        button_register = tk.Button(root, text="registro", font=("Arial", 12),command=self.list_clear).place(x=20, y=55)
+        button_edit = tk.Button(root, text="editar", font=("Arial", 12),command=self.list_clear).place(x=120, y=55)
+        button_eliminar = tk.Button(root, text="editar", font=("Arial", 12),command=self.list_clear).place(x=210, y=55)
+
+        query = "SELECT id_categoria,nombre,descripcion FROM categoria"
+        self.mysqlcursor.execute(query)
+        rows = self.mysqlcursor.fetchall()
+        self.list_update(rows)
